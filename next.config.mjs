99b4dadb0dbcbbe874b7aff20531eccf47a6012e
jafarOlamide <jafarolamidekale@gmail.com>
2026-02-24 18:37:50 +0000

    remove basepath in next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@ import { withContentCollections } from "@content-collections/next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/jafarolamide.github.io",
   output: "export",
   reactStrictMode: true,
   async headers() {
